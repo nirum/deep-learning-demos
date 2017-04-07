c433cc5c2a20d1c3dfc5f2b7921a8a8b9c03ceeb
Niru Maheswaranathan <nirum@stanford.edu>
2017-04-07 01:51:52 -0700

    adds feedback alignment

--- layers.py
+++ layers.py
@@ -1,7 +1,7 @@
 import numpy as np
-from descent.algorithms import adam
 from toolz import compose
 from utils import sigmoid
+from itertools import count
 
 __all__ = ['Network', 'Layer']
 
@@ -55,21 +55,28 @@ class Network:
 
 
 class Layer:
-    def __init__(self, dims, learning_rate=1e-3, activation=sigmoid):
+    def __init__(self, dims, learning_rate=1e-3, activation=sigmoid, feedback_alignment=False):
         """A single fully connected layer"""
 
         # initialize parameters
         weight_scaling = 0.1 / np.sqrt(np.prod(dims))
         bias_scaling = 0.1 / np.sqrt(dims[0])
-        self.weights =  np.random.randn(*dims) * weight_scaling
-        self.bias = np.random.randn(dims[0],1) * bias_scaling
+        self.weights = np.random.randn(*dims) * weight_scaling
+        self.feedback = np.random.randn(*self.weights.T.shape)
+        self.feedback_alignment = feedback_alignment
+        self.bias = np.random.randn(dims[0], 1) * bias_scaling
 
         # initialize nonlinearity
         self.activation = activation
 
         # initialize optimizer for each parameter
-        self.weights_opt = adam(self.weights, lr=learning_rate)
-        self.bias_opt = adam(self.bias, lr=learning_rate)
+        self.weights_opt = adam(lr=learning_rate)
+        self.weights_opt.send(None)
+        self.weights_opt.send(self.weights)
+
+        self.bias_opt = adam(lr=learning_rate)
+        self.bias_opt.send(None)
+        self.bias_opt.send(self.bias)
 
         # if true, then learning is turned on, and the weights are updated
         self.active = True
@@ -105,8 +112,51 @@ class Layer:
             self.update(dW, db)
 
         # pass back the new error
-        return self.weights.T @ delta
+        if self.feedback_alignment:
+            err = self.feedback @ delta
+        else:
+            err = self.weights.T @ delta
+
+        return err
 
     def update(self, dW, db):
-        self.weights = self.weights_opt(dW)
-        self.bias = self.bias_opt(db)
+        self.weights = self.weights_opt.send(dW)
+        self.bias = self.bias_opt.send(db)
+
+
+def adam(lr=1e-3, beta=(0.9, 0.999), epsilon=1e-8):
+    """
+    ADAM
+
+    Parameters
+    ----------
+    lr : float, optional
+        Learnin rate (Default: 1e-3)
+
+    beta : (float, float)
+        (Default: (0.9, 0.999))
+
+    epsilon : float
+        (Default: 1e-8)
+    """
+    xk = yield
+    mk = np.zeros_like(xk)
+    vk = np.zeros_like(xk)
+    b1, b2 = beta
+
+    for k in count(start=1):
+        grad = yield xk
+
+        mk *= b1
+        mk += (1. - b1) * grad
+
+        # update velocity
+        vk *= b2
+        vk += (1. - b2) * (grad ** 2)
+
+        # normalize
+        momentum_norm = mk / (1 - b1 ** k)
+        velocity_norm = np.sqrt(vk / (1 - b2 ** k))
+
+        # gradient descent update
+        xk -= lr * momentum_norm / (epsilon + velocity_norm)
--- toynn.py
+++ toynn.py
@@ -10,17 +10,22 @@ from layers import Layer, Network
 from tqdm import trange
 
 
-def build_fullyconnected(norm=np.inf, nhidden=5):
+def build_fullyconnected(norm=np.inf, nhidden=5, fa1=True, fa2=True):
 
     # generate data (in a box)
     X, y = generate_data(norm=norm)
 
     # build network
-    L1 = Layer((nhidden, 2))
-    L2 = Layer((1, nhidden))
+    L1 = Layer((nhidden, 2), feedback_alignment=fa1)
+    L2 = Layer((1, nhidden), feedback_alignment=fa2)
     net = Network(X, y, [L1, L2])
 
     return net
+
+
+def sim(norm, nh, fa1, fa2, numiter=10000):
+    net = build_fullyconnected(norm=norm, nhidden=nh, fa1=fa1, fa2=fa2)
+    return np.array([net() for _ in trange(numiter)]), net
 
 
 if __name__ == '__main__':
@@ -28,12 +33,7 @@ if __name__ == '__main__':
     # norm ball for generate toy data
     norm = np.inf
 
-    # build the network
-    net = build_fullyconnected(norm=norm)
-
-    # train (should take ~10s)
-    numiter = 20000
-    objective = np.array([net() for _ in trange(numiter)])
+    objective, net = sim(norm, 50, True, True)
 
     # predicted class labels (on held out data)
     X_holdout, y_holdout = generate_data(norm=norm, nsamples=5000)
@@ -41,7 +41,7 @@ if __name__ == '__main__':
 
     # plot the training curve
     plt.figure()
-    plt.plot(np.arange(numiter), objective)
+    plt.plot(np.arange(objective.size), objective)
     plt.xlabel('Iteration ($k$)')
     plt.ylabel('Training error ($f(k)$)')
 
